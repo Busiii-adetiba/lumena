d8e33e923e72ecfa5358fee2eff1ab310eaf0b84
debbie <deborahamoni0@gmail.com>
2026-09-17 11:38:35 +0000

    feat(server): expose POST /webhooks endpoint for subscription management

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -3,6 +3,7 @@ export { FeeSponsorService } from "./fee-sponsor/service.js";
 export { SponsorMonitorService } from "./fee-sponsor/monitor.js";
 export { PolicyEngine } from "./policy/engine.js";
 export { InMemoryPolicyStore, RedisPolicyStore } from "./policy/store.js";
+export { WebhookDispatcher, type WebhookDispatcherOpts } from "./webhook/dispatcher.js";
 export { createServer, type ServerOpts, type ServerResult } from "./server.js";
 export { EnvSigner } from "./signers/EnvSigner.js";
 export {
